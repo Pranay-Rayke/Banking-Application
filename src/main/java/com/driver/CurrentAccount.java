1de6c98a602fadbc327f20dcc9d2d891e083185e
Pranay Rayke <pranayrayke@gmail.com>
2023-03-24 10:36:34 +0530

    Banking Assignment

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,11 +1,19 @@
 package com.driver;
 
+import java.util.Locale;
+
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+        super(name,balance,5000.00);
+        this.tradeLicenseId = tradeLicenseId.toUpperCase();
 
+        if(balance<5000)
+        {
+            throw new Exception("Insufficient Balance");
+        }
     }
 
     public void validateLicenseId() throws Exception {
